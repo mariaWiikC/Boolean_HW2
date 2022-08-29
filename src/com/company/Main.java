1276e4e72d4364e6538368069fd5372920100e79
Maria Wiik <wiikcandido.maria@syk.fi>
2022-08-29 14:30:00 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        boolean shining = true;
+        int time = 11;
+
+        if ((10 <= time && time <= 16 && shining) == true)
+            System.out.println("Please use sunscreen");
+    }
+}
